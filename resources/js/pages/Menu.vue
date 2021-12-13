aa2cc375bb3a9b5b57fbfd07c2161de5f7ff7f20
Jeden1102 <bum123@o2.pl>
2021-12-13 20:41:08 +0100

    add:price filter

--- resources/js/pages/Menu.vue
+++ resources/js/pages/Menu.vue
@@ -5,9 +5,30 @@
         <div class="my-4">
             <b-button  v-b-toggle.collapse-1 variant="primary"><i class="fas fa-filter"></i></b-button>
             <b-collapse id="collapse-1" class="mt-2">
-                <b-card>
-                <p class="card-text">Collapse contents Here</p>
- 
+                <b-card >
+                <div class="flex justify-evenly flex-wrap">
+                    <div>
+                        <h3>Addons:</h3>
+                        <b-form-checkbox @change="filterProducts" v-for="addon in addonsBase" :key="addon"  :value="addon"  v-model="addonsFilters">{{addon}}</b-form-checkbox>
+                    </div>
+                    <div>
+                        <h3>Size:</h3>
+                        <b-form-checkbox @change="filterProducts" v-for="size in sizeBase" :key="size"  :value="size"  v-model="sizeFilters">{{size}}</b-form-checkbox>
+                    </div>
+                    <div>
+                    <h3>Price:</h3>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">$</span>
+                        <input @keyup="filterProducts" placeholder="Min price" v-model="min" type="text" class="form-control">
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">$</span>
+                        <input @keyup="filterProducts" placeholder="Max price" v-model="max" type="text" class="form-control">
+                    </div>
+                    </div>
+
+                    </div>
+
                 </b-card>
             </b-collapse>
         </div>
@@ -20,10 +41,13 @@
                 <hr>
                 <h3>Products list of {{category.name}} category : </h3>
                 <div class="flex flex-wrap justify-center items-center">
-                    <div class="p-2 m-2  hover:bg-gray-100 cursor-pointer rounded shadow w-52 h-72 relative " v-for="product in category.products" :key="product.id" :title="product.name">
+                    <div class="p-2 m-2  hover:bg-gray-100 cursor-pointer rounded shadow w-52  relative " v-for="product in category.products" :key="product.id" :title="product.name">
                     <img class="h-3/4 w-full" src="/images/pizza-home.png"/>
                     <p class="font-bold">{{product.name}}</p>
                     <p>{{product.price}}$</p>
+                    <p>tom : {{product.tomato}}</p>
+                    <p>ch : {{product.chicken}}</p>
+                    <p>bef : {{product.beaf}}</p>
                     <button type="button" class="text-white absolute right-2 bottom-2  bg-yellow-600 hover:bg-yellow        focus:ring-4        focus:ring-green-300 font-medium rounded-lg text-sm p-2.5 text-center  dark:bg-green    dark:hover:b     g-green-700       dark:focus:ring-green-800 font-bold"><i class="far fa-eye"></i>Show</button>       
                     <button type="button" class="text-white absolute right-2 top-2   bg-green-700 hover:        bg-green-800 focus:ring-4       focus:ring-green-300 font-medium rounded-lg text-sm p-2.5 text-center  dark:bg-green    -600 dark:hover:bg-green-700      dark:focus:ring-green-800 font-bold"><i class="fas fa-cart-plus"></i></button>      
                     </div>      
@@ -44,6 +68,36 @@
                 categoryList:null,
                 productList:null,
                 productsByCategory:[],
+                addonsBase : ["tomato","cheese","paprika","beef","chicken"],
+                addonsFilters : ["tomato","cheese","paprika","beef","chicken"],
+                sizeBase : ["small","medium","large","x-large"],
+                sizeFilters : ["small","medium","large","x-large"],
+                min:null,
+                max:null,
+
+
+            }
+        },
+        methods:{
+            filterProducts(){
+                this.productsByCategory = [];
+
+                this.categoryList.forEach((el)=>{
+                    this.max = this.max=="" ? null : this.max;
+                    let filtered = this.productList.filter(product => product.category_id ==  el.id);
+                    filtered = filtered.filter(product=>Number(product.price) >= Number(this.min));
+                    filtered = this.max != null ? filtered.filter(product=>Number(product.price) <= Number(this.max)) : filtered;
+
+
+                    let productsObj = {
+                        id:el.id,
+                        name : el.name,
+                        products:filtered,
+                        amount : filtered.length
+                    }
+                    this.productsByCategory.push(productsObj);
+                })
+                console.log(this.productsByCategory);
             }
         },
         mounted(){
@@ -73,13 +127,14 @@
                     }
                     self.productsByCategory.push(productsObj);
                 })
-                console.log(self.productsByCategory)
             })
             .catch(function (error) {
                 console.log(error);
             })
             .then(function () {
             });
+            //check
+
         }
     }
 </script>
