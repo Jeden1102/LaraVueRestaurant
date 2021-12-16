8fb93445fd9cf4f1a411aba67245737327a76dc1
Jeden1102 <bum123@o2.pl>
2021-12-16 21:13:25 +0100

    hotfix

--- database/migrations/2021_11_28_112814_create_products_table.php
+++ database/migrations/2021_11_28_112814_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image_url');
             $table->string('name');
             $table->string('desc')->nullable();
             $table->decimal('price')->nullable();
